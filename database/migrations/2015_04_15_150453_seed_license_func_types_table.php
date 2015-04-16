660121912bc1ad13d65a6a200208b5337bd1a929
Miroslav Svitok <mirosvitok@gmail.com>
2015-04-16 13:01:33 +0200

    Fix of lic func  types seeding migrate

--- database/migrations/2015_04_15_150453_seed_license_func_types_table.php
+++ database/migrations/2015_04_15_150453_seed_license_func_types_table.php
@@ -16,7 +16,7 @@ class SeedLicenseFuncTypesTable extends Migration {
         LicenseFuncType::create(['name' => LicenseFuncType::TYPE_FULL, 'order' => 2]);
         LicenseFuncType::create(['name' => LicenseFuncType::TYPE_CALL_ME_CLIENT, 'order' => 3]);
         LicenseFuncType::create(['name' => LicenseFuncType::TYPE_POOL_CLIENT, 'order' => 4]);
-        lliLicenseFuncType::create(['name' => LicenseFuncType::TYPE_COMPANY_CLIENT, 'order' => 5]);
+        LicenseFuncType::create(['name' => LicenseFuncType::TYPE_COMPANY_CLIENT, 'order' => 5]);
 	}
 
 	/**
