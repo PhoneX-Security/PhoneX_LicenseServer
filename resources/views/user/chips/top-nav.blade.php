ac20e50af96056fbc323c9445725865fb320a2fb
Miroslav Svitok <mirosvitok@gmail.com>
2015-06-30 13:57:10 +0200

    Tab switcher

--- resources/views/user/chips/top-nav.blade.php
+++ resources/views/user/chips/top-nav.blade.php
@@ -1,5 +1,5 @@
 <ul class="nav nav-tabs">
-    <li role="presentation" class="active"><a href="{{ \URL::route('users.show', [ $user->id ]) }}">Details</a></li>
-    <li role="presentation"><a href="{{ \URL::route('users.licenses', [ $user->id ]) }}">Licenses</a></li>
-    <li role="presentation"><a href="{{ \URL::route('users.cl', [ $user->id ]) }}">Contact List</a></li>
+    <li role="presentation" @if(Route::is('users.show')) class="active" @endif ><a href="{{ \route('users.show', [ $user->id ]) }}">Details</a></li>
+    <li role="presentation" @if(Route::is('users.licenses')) class="active" @endif><a href="{{ route('users.licenses', [ $user->id ]) }}">Licenses</a></li>
+    <li role="presentation" @if(Route::is('users.cl')) class="active" @endif><a href="{{ route('users.cl', [ $user->id ]) }}">Contact List</a></li>
 </ul>
