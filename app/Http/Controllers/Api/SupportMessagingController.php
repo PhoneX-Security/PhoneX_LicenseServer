a0b99bd631c2ba4742f03ca76cd3b647ead8e5ba
Miroslav Svitok <mirosvitok@gmail.com>
2015-08-02 20:36:12 +0200

    messaging support api improved
    Signed-off-by: Miroslav Svitok <mirosvitok@gmail.com>

--- app/Http/Controllers/Api/SupportMessagingController.php
+++ app/Http/Controllers/Api/SupportMessagingController.php
@@ -11,18 +11,23 @@ class SupportMessagingController extends Controller {
     public function batch()
     {
         // just example by now
-        $batch = [];
+        $batch = new \stdClass();
+        $batch->batch_id = 5734;
+        $batch->data = [];
+
         $obj1 = new \stdClass();
+        $obj1->type = 'welcome-message';
         $obj1->locale = 'en';
         $obj1->sips = ["adevel624@phone-x.net", "test318@phone-x.net"];
-        $obj1->message = "Test message for adevel624 and test318.";
-        $batch[] = $obj1;
+        $obj1->message = "Test message for adevel624 and test318. Welcome to PhoneX!";
+        $batch->data[] = $obj1;
 
         $obj2 = new \stdClass();
         $obj2->locale = 'cs';
+        $obj1->type = 'license-expires-in-day';
         $obj2->sips = ["test322@phone-x.net"];
-        $obj2->message = "Testovací zpráva pro test322.";
-        $batch[] = $obj2;
+        $obj2->message = "Testovací zpráva pro test322 - za den ti expiruje licence kreténe, kup si novou.";
+        $batch->data[] = $obj2;
 
         return json_encode($batch);
     }
