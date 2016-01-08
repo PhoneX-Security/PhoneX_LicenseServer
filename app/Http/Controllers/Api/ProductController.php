0d82945e7a976248c2d1ada9ad29585f33b6cf1b
Miroslav Svitok <mirosvitok@gmail.com>
2016-01-08 15:27:33 +0100

    Product controller changes

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -64,94 +64,242 @@ class ProductController extends Controller {
             abort(401);
         }
 
-        // Differentiate products according to platform
-        $platform = $request->get('platform', 'apple');
-        $isApple = $platform === 'apple';
-
         // Set locale according to GET parameter to retrieve localized product names
         $locales = explode(',', $request->get('locales', 'en'));
         $locale = $locales[0];
         app()->setLocale($locale);
 
-//        $user = User::findByUsername('test-internal3');
+        // Products can differ for both platforms
+        if ($this->isAndroidPlatform($request)){
+            // ANDROID
+            // legacy products
+//            $basicProductNames = ['inapp.subs.basic.month'];
+//            $premiumProductNames = ['inapp.subs.premium.e10'];
+
+            $basicProductNames = ['android.subs.basic2.month'];
+            $premiumProductNames = ['android.subs.business.month'];
+            $packageNames = ['inapp.cons.call30', 'inapp.cons.call60'];
+        } else {
+            // APPLE
+            // legacy and also the most recent ones
+//            $basicProductNames = ['inapp.subs.basic.month', 'ios.subs.basic3.month'];
+            $basicProductNames = ['ios.subs.basic3.month'];
+//            $premiumProductNames = ['ios.subs.business.month', 'inapp.subs.premium.e20'];
+            $premiumProductNames = ['ios.subs.business.month'];
+            $packageNames = ['inapp.cons.call30', 'inapp.cons.call60'];
+        }
+
+        $basicProducts = $this->loadProductsFromNames($basicProductNames);
+        $premiumProducts = $this->loadProductsFromNames($premiumProductNames);
+        $consumablePackages = $this->loadProductsFromNames($packageNames);
+
+        // load active subscriptions
+        $subscriptionLicenses = $this->loadActiveSubscriptionLicenses($user);
 
         $obj = new \stdClass();
         $obj->version = self::VERSION;
         $obj->products = [];
 
-        $licenses = $user->activeLicenseProducts;
-
-        $subscriptionLicenses = $licenses->filter(function($lic){
-            return !$lic->product->isConsumable();
-        });
-
-        $subBasic = Product::findByNameWithPerms("inapp.subs.basic.month");
-        $subBasic->loadPermissionsFromParentIfMissing();
-        $subPremium = null;
-        if ($isApple){
-            $subPremium = Product::findByNameWithPerms("inapp.subs.premium.e20");
-        } else {
-            // New product had to be released for Google, price cannot be changed
-            $subPremium = Product::findByNameWithPerms("inapp.subs.premium.e10");
-        }
-
-//
-        $subPremium->loadPermissionsFromParentIfMissing();
-
-        $consCall30 = Product::findByNameWithPerms("inapp.cons.call30");
-        $consCall30->loadPermissionsFromParentIfMissing();
-        $consCall60 = Product::findByNameWithPerms("inapp.cons.call60");
-        $consCall60->loadPermissionsFromParentIfMissing();
-
-//        $consFiles25 = Product::findByNameWithPerms("inapp.cons.files25");
-//        $consFiles25->loadPermissionsFromParentIfMissing();
-//        $consFiles50 = Product::findByNameWithPerms("inapp.cons.files50");
-//        $consFiles50->loadPermissionsFromParentIfMissing();
-
-        // if no there are no active licenses
         if ($subscriptionLicenses->count() == 0){
-            // offer basic
-            $obj->products[] = $subBasic;
+            // offer basic products
+            $this->addProducts($obj->products, $basicProducts);
+            // From January 2015, offer premium products right away
+            $this->addProducts($obj->products, $premiumProducts);
 
-            // if user had basic previously, offer premium as well
-            $pastLicenses = $user->pastLicenseProducts;
-            $hadBasic = false;
-            foreach ($pastLicenses as $lic){
-                if ($lic->product->name == $subBasic->name){
-                    $hadBasic = true;
-                    break;
-                }
-            }
+            //Temporary - for iOS, offer premium by default
+//            if (!$this->isAndroidPlatform($request)){
+//                $this->addProducts($obj->products, $premiumProducts);
+//            } else {
+//                // if user had basic previously, offer premium as well
+//                $pastLicenses = $user->pastLicenseProducts;
+//                if ($this->hasProductInLicenses($pastLicenses, $basicProducts)){
+//                    $this->addProducts($obj->products, $premiumProducts);
+//                }
+//            }
 
-            if ($hadBasic){
-                $obj->products[] = $subPremium;
-            }
 
             return json_encode($obj);
         }
 
+        // If there are some licenses purchased, offer differs according to whether user has already purchased basic license
         $containsOnlyBasic = true;
         foreach ($subscriptionLicenses as $lic){
-            if ($lic->product->name != $subBasic->name){
+            if(!in_array($lic->product->name, $basicProductNames)){
                 $containsOnlyBasic = false;
+                break;
             }
         }
 
-        // only basic subscription - in this case, offer consumables + premium
+        // Only basic subscription - in this case, offer consumables + premium
         if ($containsOnlyBasic){
-//            $obj->products[] = $subBasic;
-            $obj->products[] = $subPremium;
-            $obj->products[] = $consCall30;
-            $obj->products[] = $consCall60;
-//            $obj->products[] = $consFiles25;
-//            $obj->products[] = $consFiles50;
+            $this->addProducts($obj->products, $premiumProducts);
+            $this->addProducts($obj->products, $consumablePackages);
             return json_encode($obj);
         } else {
-            // all other cases (legacy etc), only offer premium
-//            $obj->products[] = $subPremium;
+            // all other cases (legacy etc), offer only premium
+            $this->addProducts($obj->products, $premiumProducts);
             return json_encode($obj);
         }
     }
+
+    /**
+     * @param $licenses (eloquent model)
+     * @param array $products
+     * @return bool Returns true if at least one of the products is contained in licenses
+     */
+    private function hasProductInLicenses(\ArrayAccess $licenses, array $products)
+    {
+        foreach($products as $product){
+            foreach($licenses as $lic){
+                if ($lic->product->name == $product->name){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private function addProducts(array &$where, array $products)
+    {
+        $where = array_merge($where, $products);
+    }
+
+
+    private function loadProductsFromNames(array $names)
+    {
+        $res = [];
+        foreach($names as $name){
+            $product = Product::findByNameWithPerms($name);
+            $product->loadPermissionsFromParentIfMissing();
+            $res[] = $product;
+        }
+        return $res;
+    }
+
+    private function loadActiveSubscriptionLicenses(User $user)
+    {
+        return $user->activeLicenseProducts->filter(function($lic){
+            return !$lic->product->isConsumable();
+        });
+    }
+
+
+    /**
+     * Android sends "google" as a platform
+     * everything else is
+     * @param Request $request
+     * @return bool
+     */
+    private function isAndroidPlatform(Request $request)
+    {
+        if ($request->has('platform') && $request->get('platform') === 'google'){
+            return true;
+        }
+        return false;
+    }
+
+    // OLD CODE
+//    public function getAvailableProducts(Request $request)
+//    {
+//        $user = $request->attributes->get(MiddlewareAttributes::CLIENT_CERT_AUTH_USER);
+//        if (!$user){
+//            abort(401);
+//        }
+//        return $this->getAvailableProductsTest($request);
+//
+////        if ($user->username == 'test-internal3'){
+////
+////        }
+//
+//
+//        // Differentiate products according to platform
+//        $platform = $request->get('platform', 'apple');
+//        $isApple = $platform === 'apple';
+//
+//        // Set locale according to GET parameter to retrieve localized product names
+//        $locales = explode(',', $request->get('locales', 'en'));
+//        $locale = $locales[0];
+//        app()->setLocale($locale);
+//
+////        $user = User::findByUsername('test-internal3');
+//
+//        $obj = new \stdClass();
+//        $obj->version = self::VERSION;
+//        $obj->products = [];
+//
+//        $licenses = $user->activeLicenseProducts;
+//
+//        $subscriptionLicenses = $licenses->filter(function($lic){
+//            return !$lic->product->isConsumable();
+//        });
+//
+//        $subBasic = Product::findByNameWithPerms("inapp.subs.basic.month");
+//        $subBasic->loadPermissionsFromParentIfMissing();
+//        $subPremium = null;
+//        if ($isApple){
+//            $subPremium = Product::findByNameWithPerms("inapp.subs.premium.e20");
+//        } else {
+//            // New product had to be released for Google, price cannot be changed
+//            $subPremium = Product::findByNameWithPerms("inapp.subs.premium.e10");
+//        }
+//
+////
+//        $subPremium->loadPermissionsFromParentIfMissing();
+//
+//        $consCall30 = Product::findByNameWithPerms("inapp.cons.call30");
+//        $consCall30->loadPermissionsFromParentIfMissing();
+//        $consCall60 = Product::findByNameWithPerms("inapp.cons.call60");
+//        $consCall60->loadPermissionsFromParentIfMissing();
+//
+////        $consFiles25 = Product::findByNameWithPerms("inapp.cons.files25");
+////        $consFiles25->loadPermissionsFromParentIfMissing();
+////        $consFiles50 = Product::findByNameWithPerms("inapp.cons.files50");
+////        $consFiles50->loadPermissionsFromParentIfMissing();
+//
+//        // if no there are no active licenses
+//        if ($subscriptionLicenses->count() == 0){
+//            // offer basic
+//            $obj->products[] = $subBasic;
+//
+//            // if user had basic previously, offer premium as well
+//            $pastLicenses = $user->pastLicenseProducts;
+//            $hadBasic = false;
+//            foreach ($pastLicenses as $lic){
+//                if ($lic->product->name == $subBasic->name){
+//                    $hadBasic = true;
+//                    break;
+//                }
+//            }
+//
+//            if ($hadBasic){
+//                $obj->products[] = $subPremium;
+//            }
+//
+//            return json_encode($obj);
+//        }
+//
+//        $containsOnlyBasic = true;
+//        foreach ($subscriptionLicenses as $lic){
+//            if ($lic->product->name != $subBasic->name){
+//                $containsOnlyBasic = false;
+//            }
+//        }
+//
+//        // only basic subscription - in this case, offer consumables + premium
+//        if ($containsOnlyBasic){
+////            $obj->products[] = $subBasic;
+//            $obj->products[] = $subPremium;
+//            $obj->products[] = $consCall30;
+//            $obj->products[] = $consCall60;
+////            $obj->products[] = $consFiles25;
+////            $obj->products[] = $consFiles50;
+//            return json_encode($obj);
+//        } else {
+//            // all other cases (legacy etc), only offer premium
+////            $obj->products[] = $subPremium;
+//            return json_encode($obj);
+//        }
+//    }
 
     /**
      * List arbitrary user's products by providing license ids
